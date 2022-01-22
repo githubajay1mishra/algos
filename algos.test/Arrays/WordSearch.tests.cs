02d06e8e6e9c5cb0e37638f23a95dec7d580408a
ajay mishra <ajay1.mishra@gmail.com>
2022-01-22 17:26:56 -0500

    Word Search algo test
    Given an m x n grid of characters board and a string word, return true if word exists in the grid.
    
    The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.

---
+++ algos.test/Arrays/WordSearch.tests.cs
@@ -0,0 +1,36 @@
+﻿using algos.Arrays;
+using Xunit;
+
+namespace algos.test.Arrays
+{
+
+    public class WordSearchTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = new char[][]
+            {
+                new char[]{ 'A','B','C','E' }, 
+                new char[]{'S', 'F', 'C', 'S' },
+                new char[]{'A', 'D', 'E', 'E' }
+            };
+
+            var wordSearch = new WordSearch();
+            Assert.True(wordSearch.Exist(input, "SEE"));
+            Assert.True(wordSearch.Exist(input, "ABCCED"));
+            Assert.False(wordSearch.Exist(input, "ABCB"));
+
+            var input2 = new char[][]
+          {
+                new char[]{ 'A','B','C','E' },
+                new char[]{'S', 'F', 'E', 'S' },
+                new char[]{'A', 'D', 'E', 'E' }
+          };
+            Assert.True(wordSearch.Exist(input2, "ABCESEEEFS"));
+          
+
+        }
+
+    }
+}
---
+++ algos/Arrays/WordSearch.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace algos.Arrays
+{
+    public class WordSearch
+    {
+        public bool Exist(char[][] board, string word)
+        {
+            for (int row = 0; row < board.Length; row++)
+            {
+                for (int col = 0; col < board[row].Length; col++)
+                {
+                    var exists = Exist(board, word, 0, new Cell(row, col), new List<string>());
+                    if (exists) return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        private bool Exist(char[][] board,
+                           string word,
+                           int indexInWord,
+                           Cell current,
+                           List<string> seen)
+        {
+
+            if (indexInWord == word.Length)
+            {
+                return true;
+            }
+
+
+
+            var currentCharInBoard = board[current.row][current.col];
+            if (currentCharInBoard != word[indexInWord])
+            {
+                return false;
+            }
+
+            if (indexInWord == word.Length - 1)
+            {
+                return true;
+            }
+
+
+
+
+            var result = false;
+            Func<Cell, string> toSeenKey = (cell) => board[cell.row][cell.col] + $" {cell.row}:{cell.col}";
+            var seenKey = toSeenKey(current);
+            seen.Add(seenKey);
+
+
+
+            var nextCells = AdjacentCells(current, board, seen);
+            var nextCellsAsKeys = nextCells.Select(x => toSeenKey(x));
+
+
+
+            foreach (var nextCell in nextCells)
+            {
+                if (Exist(board, word, indexInWord + 1, nextCell, seen))
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            seen.Remove(seenKey);
+
+            return result;
+        }
+
+        private List<Cell> AdjacentCells(Cell current, char[][] board, List<String> seen)
+        {
+            var cells = new List<Cell>();
+
+            if (current.row - 1 >= 0)
+            {
+                cells.Add(new Cell(current.row - 1, current.col));
+            }
+
+            if (current.row + 1 < board.Length)
+            {
+                cells.Add(new Cell(current.row + 1, current.col));
+            }
+
+            if (current.col - 1 >= 0)
+            {
+                cells.Add(new Cell(current.row, current.col - 1));
+            }
+
+            if (current.col + 1 < board[0].Length)
+            {
+                cells.Add(new Cell(current.row, current.col + 1));
+            }
+
+            Predicate<Cell> remove = (Cell cell) =>
+            {
+                var key = board[cell.row][cell.col] + $" {cell.row}:{cell.col}";
+                return seen.Contains(key);
+            };
+
+            cells.RemoveAll(remove);
+
+
+
+
+
+            return cells;
+        }
+
+
+    }
+
+    internal struct Cell
+    {
+        public int row;
+        public int col;
+
+        public Cell(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Cell other &&
+                   row == other.row &&
+                   col == other.col;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1720622044;
+            hashCode = hashCode * -1521134295 + row.GetHashCode();
+            hashCode = hashCode * -1521134295 + col.GetHashCode();
+            return hashCode;
+        }
+
+        public void Deconstruct(out int row, out int col)
+        {
+            row = this.row;
+            col = this.col;
+        }
+
+        public static implicit operator (int row, int col)(Cell value)
+        {
+            return (value.row, value.col);
+        }
+
+        public static implicit operator Cell((int row, int col) value)
+        {
+            return new Cell(value.row, value.col);
+        }
+    }
+}

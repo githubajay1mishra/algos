01d1c9d73c10eb3a617213e8e4765109e746772a
ajay mishra <ajay1.mishra@gmail.com>
2022-01-29 21:01:30 -0500

    Combination Sum One

---
+++ algos.test/BackTracking/CombinationSumTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using algos.BackTracking;
+using Xunit;
+
+namespace algos.test.BackTracking
+{
+    public class CombinationSumOneTests
+    {
+        private readonly CombinationSumOne sol;
+        public CombinationSumOneTests()
+        {
+            sol = new CombinationSumOne();
+        }
+
+        [Fact]
+        public void ValidateExample1()
+        {
+            var result = sol.CombinationSum(new int[] { 2, 3, 6, 7 }, 7);
+            var expected = new List<List<int>>
+            {
+                new List<int> { 2, 2, 3 },
+                new List<int> { 7 }
+            };
+            Assert.Equal(expected, result);
+
+        }
+
+    
+    }
+}
---
+++ algos/BackTracking/CombinationSumOne.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace algos.BackTracking
+{
+    public class CombinationSumOne
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            var combinations = new List<IList<int>>();
+            CombinationSumInternal(candidates, target, 0, new List<int>(), combinations);
+            return combinations;
+        }
+
+        private void CombinationSumInternal(int[] candidates, int target, int current, IList<int> combination, IList<IList<int>> combinations)
+        {
+            if (target < 0) return;
+            if(current >= candidates.Length) return;
+
+            if(target == 0)
+            {
+                combinations.Add(new List<int>(combination));
+                return;
+            }
+
+            for(int index = current; index < candidates.Length; index++)
+            {
+                combination.Add(candidates[index]);
+                CombinationSumInternal(candidates, target-candidates[index], index, combination, combinations);
+                combination.RemoveAt(combination.Count - 1);
+            }
+
+        }
+
+       
+    }
+}
+

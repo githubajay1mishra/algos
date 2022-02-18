2db0a0c80ac4b96e188c09b33c4577237aa1b19f
ajay mishra <ajay1.mishra@gmail.com>
2022-02-17 21:14:24 -0500

    Maximal rectangle

---
+++ algos.test/Stack/MaximalRectangleMatrixTests.cs
@@ -0,0 +1,35 @@
+﻿using algos.Stacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace algos.test.Stack
+{
+    public class MaximalRectangleMatrixTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+
+        public void Validate(char[][] data, int expected)
+        {
+            var sol = new MaximalRectangleMatrix();
+            Assert.Equal(expected, sol.MaximalRectangle(data));
+        }
+
+        public static IEnumerable<object[]> Data => new List<object[]>
+        {
+            new object[]{ new char[][] { new char[] { '1' } }, 1},
+            new object[]{ new char[][] { new char[] { '0' } }, 0},
+            new object[]{ new char[][] {
+            new char[] {'1','0','1','0','0' },
+            new char[] {'1','0','1','1','1' },
+            new char[] {'1','1','1','1','1' },
+            new char[] {'1','0','0','1','0' }
+            }, 6},
+
+        };
+    }
+}
---
+++ algos/Stacks/MaximalRectangleMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace algos.Stacks
+{
+    public class MaximalRectangleMatrix
+    {
+        public int MaximalRectangle(char[][] matrix)
+        {
+            var result = matrix.Select(x => x.Select(value => (int)char.GetNumericValue(value)).ToArray()).ToArray();
+            for(int row = 1; row < result.Length; row++) {
+                for (int col = 0; col < result[row].Length; col++) { 
+                    result[row][col] = result[row][col] == 1 ? result[row-1][col] + 1 : result[row][col];
+                }
+            }
+            var maxRectangleArea = 0;
+            foreach (var row in result) {
+                maxRectangleArea = Math.Max(maxRectangleArea, CalculateMaxArea(row));
+            }
+            return maxRectangleArea;
+        }
+
+        public int CalculateMaxArea(int[] heights)
+        {
+            int maxArea = 0;
+            var stackHeights = new Stack<int>();
+            for (int i = 0; i < heights.Length; i++)
+            {
+                while(stackHeights.Count > 0 && heights[i] <= heights[stackHeights.Peek()]){
+
+                    var prevTop = stackHeights.Pop();
+                    var height = heights[prevTop];
+                    var width = stackHeights.Count == 0 ? i: i - 1 - stackHeights.Peek();
+                    maxArea = Math.Max(maxArea, height * width);
+                }
+
+                stackHeights.Push(i);
+            }
+            while (stackHeights.Count > 0) {
+                var prevTop = stackHeights.Pop();
+                var height = heights[prevTop];
+                var width = stackHeights.Count == 0 ? heights.Length: heights.Length - 1 - stackHeights.Peek();
+                maxArea = Math.Max(maxArea, height * width);
+            }
+            return maxArea;
+        }
+    }
+}

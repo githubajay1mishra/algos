379278c1fb7a538c62c982ffe11a44b84ae75680
ajay mishra <ajay1.mishra@gmail.com>
2022-02-17 20:13:57 -0500

    Largest Histogram

---
+++ algos.test/Stack/LargestAreaHistogramTests.cs
@@ -0,0 +1,25 @@
+﻿using algos.Stacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace algos.test.Stack
+{
+    public class LargestAreaHistogramTests
+    {
+        [Theory]
+        [InlineData(new int[] {2, 1, 5, 6, 2, 3 }, 10)]
+        [InlineData(new int[] { 1, 1 }, 2)]
+        [InlineData(new int[] { 2, 1, 2 }, 3)]
+        [InlineData(new int[] { 2, 0, 1, 2 }, 2)]
+        [InlineData(new int[] { 5, 4, 1, 2 }, 8)]
+        [InlineData(new int[] { 5, 4, 4, 6, 3, 2, 9, 5, 4, 8, 1, 0, 0, 4, 7, 2 }, 20)]
+        public void Validate(int[] nums, int expected)
+        {
+            Assert.Equal(expected, new LargestAreaHistogram().LargestRectangleArea(nums));
+        }
+    }
+}
---
+++ algos/Stacks/LargestAreaHistogram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace algos.Stacks
+{
+    public class LargestAreaHistogram 
+    {
+        public int LargestRectangleArea(int[] heights)
+        {
+            var stackHeights = new Stack<int>();
+            var result = 0;
+            for(int i = 0; i < heights.Length; i++)
+            {
+                while(stackHeights.Count > 0 && heights[i] <= heights[stackHeights.Peek()])
+                {
+                    var prevTop = stackHeights.Pop();
+                    var barHeight = heights[prevTop];
+                    var width = stackHeights.Count == 0 ? i : (i-1) - stackHeights.Peek();
+                    var area = barHeight * (width);
+                    result = Math.Max(area, result);
+
+                }
+
+                stackHeights.Push(i);
+            }
+         
+
+            while(stackHeights.Count > 0)
+            {
+                
+                var prevTop = stackHeights.Pop();
+                var barHeight = heights[prevTop];
+                var width = stackHeights.Count == 0 ? heights.Length : (heights.Length - 1) - stackHeights.Peek();
+                var area = barHeight * width;
+                result = Math.Max(area, result);
+            }
+
+            return result;
+
+            
+        }
+    }
+}

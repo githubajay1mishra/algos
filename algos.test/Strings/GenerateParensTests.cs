ad7affcfa6ecfd713246ec03a5113be542ded39c
ajay mishra <ajay1.mishra@gmail.com>
2022-02-26 13:33:48 -0500

    Parens generator

---
+++ algos.test/Strings/GenerateParensTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using algos.Strings;
+
+namespace algos.test.Strings
+{
+    public class GenerateParensTests
+    {
+        [Theory]
+        [InlineData(1, new string[] { "()" })]
+        [InlineData(2, new string[] { "()()", "(())" })]
+        [InlineData(3, new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" })]
+        public void Validate(int num, string[] expected)
+        {
+            var expectedlist = expected.ToList();
+            expectedlist.Sort();
+            var result = new ParensGenerator().GenerateParenthesis(num).ToList();
+            result.Sort();
+            Assert.Equal(expectedlist, result);
+        }
+    }
+
+}
---
+++ algos/Strings/ParensGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algos.Strings
+{
+    public class ParensGenerator
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new HashSet<string>();
+            GenerateParensInternal(n, result, "", 0, 0, new HashSet<string>());
+            return new List<string>(result);
+        }
+
+        private void GenerateParensInternal(int num, HashSet<string> result, string prefix, int leftAddedCount, int rightAddedCount, HashSet<string> memo)
+        {
+
+            if(leftAddedCount == num && rightAddedCount == num)
+            {
+                result.Add(prefix);
+                return;
+            }
+
+            if (memo.Contains(prefix))
+            {
+                return;
+            }
+
+            memo.Add(prefix);
+
+            if(leftAddedCount == num)
+            {
+                GenerateParensInternal(num, result, prefix + ')', leftAddedCount, rightAddedCount + 1, memo);
+                return;
+            }
+            
+            if(leftAddedCount == rightAddedCount)
+            {
+                GenerateParensInternal(num, result, prefix + '(', leftAddedCount + 1, rightAddedCount, memo);
+                return;
+
+            }
+
+            GenerateParensInternal(num, result, prefix + '(', leftAddedCount + 1, rightAddedCount, memo);
+            GenerateParensInternal(num, result, prefix + ')', leftAddedCount, rightAddedCount + 1, memo);
+
+        }
+    }
+}
